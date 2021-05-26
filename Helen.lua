b6c6454cc303751911db52856b88c8df2ebd40f3
VvVvFv <73939109+VvVvFv@users.noreply.github.com>
2021-05-26 07:50:58 +0300

    Add files via upload

--- Helen.lua
+++ Helen.lua
@@ -5029,6 +5029,25 @@ end
 send(msg.chat_id_, msg.id_," 𖣏︙تم مسح جميع الاوامر التي تم اضافتها")  
 end
 end
+if text == "ترتيب الاوامر" and Constructor(msg) then
+database:set(bot_id.."Helen:Set:Cmd:Group:New1"..msg.chat_id_..":ا","ايدي")
+database:sadd(bot_id.."Helen:List:Cmd:Group:New"..msg.chat_id_,"ا")
+database:set(bot_id.."Helen:Set:Cmd:Group:New1"..msg.chat_id_..":م","رفع مميز")
+database:sadd(bot_id.."Helen:List:Cmd:Group:New"..msg.chat_id_,"م")
+database:set(bot_id.."Helen:Set:Cmd:Group:New1"..msg.chat_id_..":اد","رفع ادمن")
+database:sadd(bot_id.."Helen:List:Cmd:Group:New"..msg.chat_id_,"اد")
+database:set(bot_id.."Helen:Set:Cmd:Group:New1"..msg.chat_id_..":مد","رفع مدير")
+database:sadd(bot_id.."Helen:List:Cmd:Group:New"..msg.chat_id_,"مد")
+database:set(bot_id.."Helen:Set:Cmd:Group:New1"..msg.chat_id_..":من","رفع منشئ")
+database:sadd(bot_id.."Helen:List:Cmd:Group:New"..msg.chat_id_,"من")
+database:set(bot_id.."Helen:Set:Cmd:Group:New1"..msg.chat_id_..":اس","رفع منشئ اساسي")
+database:sadd(bot_id.."Helen:List:Cmd:Group:New"..msg.chat_id_,"اس")
+database:set(bot_id.."Helen:Set:Cmd:Group:New1"..msg.chat_id_..":تعط","تعطيل الايدي بالصوره")
+database:sadd(bot_id.."Helen:List:Cmd:Group:New"..msg.chat_id_,"تعط")
+database:set(bot_id.."Helen:Set:Cmd:Group:New1"..msg.chat_id_..":تفع","تفعيل الايدي بالصوره")
+database:sadd(bot_id.."Helen:List:Cmd:Group:New"..msg.chat_id_,"تفع")
+send(msg.chat_id_, msg.id_,"⌔︙تم ترتيب الاوامر بالشكل التالي ~\n- ايدي - ا .\n- مميز - م .\n- ادمن - اد .\n- مدير - مد . \n- منشى - من . \n- المنشئ الاساسي - اس  . \n- تعطيل الايدي بالصوره - تعط .\n- تفعيل الايدي \n بالصوره - تفع .")  
+end
 if text == "اضف امر" and Constructor(msg) then
 if AddChannel(msg.sender_user_id_) == false then
 local textchuser = database:get(bot_id..'text:ch:user')
